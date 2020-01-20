3ebad3c624881e910adbd2e887b61c765f7cef16
RENATU BONFIM <renatu_mb@yahoo.com.br>
2020-01-20 12:11:09 -0800

    Owner updated, visit entity created. Closes #26

--- pet-clinic-data/src/main/java/com/rbonfim/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/rbonfim/petclinic/model/Owner.java
@@ -1,4 +1,16 @@
 package com.rbonfim.petclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+
+    Set<Pet> petSet;
+
+    public Set<Pet> getPetSet() {
+        return petSet;
+    }
+
+    public void setPetSet(Set<Pet> petSet) {
+        this.petSet = petSet;
+    }
 }
