3f69247a40f1c27bd4337b2cd4d8e972b45eaf7d
RENATU BONFIM <renatu_mb@yahoo.com.br>
2020-01-20 13:38:22 -0800

    Created Specialty map service impl. Closes #31

---
+++ pet-clinic-data/src/main/java/com/rbonfim/petclinic/service/SpecialityService.java
@@ -0,0 +1,6 @@
+package com.rbonfim.petclinic.service;
+
+import com.rbonfim.petclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/rbonfim/petclinic/service/map/SpecialityServiceMap.java
@@ -0,0 +1,34 @@
+package com.rbonfim.petclinic.service.map;
+
+import com.rbonfim.petclinic.model.Speciality;
+import com.rbonfim.petclinic.service.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality obj) {
+        return super.save(obj);
+    }
+
+    @Override
+    public void delete(Speciality o) {
+        super.delete(o);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
