19680ade7e022e05b11dab8a8c6f0d7a6c981191
RENATU BONFIM <renatu_mb@yahoo.com.br>
2020-01-20 12:18:11 -0800

    Added contact info properties to Owner. Closes #28

--- pet-clinic-data/src/main/java/com/rbonfim/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/rbonfim/petclinic/model/Owner.java
@@ -5,6 +5,9 @@ import java.util.Set;
 public class Owner extends Person {
 
     Set<Pet> petSet;
+    private String address;
+    private String city;
+    private String telephone;
 
     public Set<Pet> getPetSet() {
         return petSet;
@@ -12,5 +15,29 @@ public class Owner extends Person {
 
     public void setPetSet(Set<Pet> petSet) {
         this.petSet = petSet;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
     }
 }
