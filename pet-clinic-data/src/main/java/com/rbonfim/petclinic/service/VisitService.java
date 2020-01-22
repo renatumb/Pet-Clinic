4676b58d1c163d3fb132481db8e3820b655dadac
RENATU BONFIM <renatu_mb@yahoo.com.br>
2020-01-21 22:12:36 -0800

    Created Visit Map Service. Closes #46

---
+++ pet-clinic-data/src/main/java/com/rbonfim/petclinic/service/VisitService.java
@@ -0,0 +1,6 @@
+package com.rbonfim.petclinic.service;
+
+import com.rbonfim.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/rbonfim/petclinic/service/map/VisitMapService.java
@@ -0,0 +1,40 @@
+package com.rbonfim.petclinic.service.map;
+
+import com.rbonfim.petclinic.model.Visit;
+import com.rbonfim.petclinic.service.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit o) {
+        super.delete(o);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+
+        if (visit.getPet() == null || visit.getPet().getId() == null || visit.getPet().getOwner() == null ||
+                visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
