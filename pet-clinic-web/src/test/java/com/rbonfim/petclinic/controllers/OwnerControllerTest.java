7cbcbf32a495f417ea76ddaaab202d3adda07d9e
RENATU BONFIM <renatu_mb@yahoo.com.br>
2020-01-25 15:44:20 -0800

    Wrote MVC tests for Owner Controller. Closes #52

---
+++ pet-clinic-web/src/test/java/com/rbonfim/petclinic/controllers/OwnerControllerTest.java
@@ -0,0 +1,60 @@
+package com.rbonfim.petclinic.controllers;
+
+import com.rbonfim.petclinic.model.Owner;
+import com.rbonfim.petclinic.service.OwnerService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashSet;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+class OwnerControllerTest {
+
+    @Mock
+    OwnerService ownerService;
+
+    @InjectMocks
+    OwnerController ownerController;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(ownerController).build();
+    }
+
+    @Test
+    void index() throws Exception {
+        when(ownerService.findAll()).thenReturn(new HashSet<Owner>());
+
+        mockMvc.perform(get("/owners")).
+                andExpect(status().is2xxSuccessful())
+                .andExpect(view().name("owners/index"));
+
+        mockMvc.perform(get("/owners/index")).
+                andExpect(status().isOk())
+                .andExpect(view().name("owners/index"))
+                .andExpect(model().attributeExists("owners"))
+                .andExpect(model().attribute("owners", hasSize(0)));
+
+    }
+
+    @Test
+    void find() throws Exception {
+
+        mockMvc.perform(get("/owners/find")).
+                andExpect(status().is2xxSuccessful())
+                .andExpect(view().name("notimplemented"));
+    }
+}
